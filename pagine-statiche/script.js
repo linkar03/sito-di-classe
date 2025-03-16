9cdf51578f886696f45a5fe50da63c69b9a55e29
linkar03 <linkar354@gmail.com>
2025-03-16 22:55:27 +0100

    Test per il funzionamento del workflow

--- pagine-statiche/script.js
+++ pagine-statiche/script.js
@@ -1,1 +1,23 @@
  
+const name = 'Giuseppa';
+const age = 25;
+
+
+console.log('Hello, World!');
+
+
+if (age == 25) {
+  console.log('Age is 25');
+}
+
+
+const unusedVariable = 'I am not used';
+
+
+x = 10;
+
+
+if (name === 'Giuseppa') {
+  console.log('Name is Giuseppa');
+}
+
