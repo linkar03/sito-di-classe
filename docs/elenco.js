bb744f1ba541ca4e8b93754bb6f663b9c7c8c724
unknown <liviuistoc1702@gmail.com>
2025-03-19 10:21:15 +0100

    modifiche essenziali

---
+++ docs/elenco.js
@@ -0,0 +1,20 @@
+const pages = [
+    [
+        "Liviu", 
+        "Pagine/Liviu.html",
+        "Risorse/immagine_profilo_liviu.png",
+        "#FF6B6B",
+        250, // dimensione
+        1.2, // velocità X
+        -0.8 // velocità Y
+    ],
+    [
+        "Alessandro", 
+        "Pagine/Alessandro.html",
+        "Risorse/immagine_profilo_ale.jpg",
+        "#4ECDC4",
+        250,
+        -1.5,
+        1.1
+    ]
+];
